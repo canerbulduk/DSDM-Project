4cfebc2ad682d5517eae3e8c0f0723213ef572b1
canerbulduk <canerbulduk@gmail.com>
2019-05-03 15:39:50 +0200

    updated

--- hotbm_logf_double/hotbm_logf_double/Main.c
+++ hotbm_logf_double/hotbm_logf_double/Main.c
@@ -4,6 +4,14 @@
 #define SUPPORT_SUBNORMALS
 #define LOG2 0x2C5C860
 
+#ifdef CHECK_LOG_FUNCTION
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpfr.h>
+#include <gmp.h>
+#endif
+
 #define CE_MACRO32(cond, a, b) ((((unsigned int) ((((int)(cond))<<31)>>31))&(a))|((~((unsigned int) ((((int)(cond))<<31)>>31)))&(b)))
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y));
@@ -18,18 +26,19 @@ typedef union {
 } float_uint_converter;
 
 
+static inline
 void range_red(unsigned char A, unsigned long long int Y0, unsigned long long int out[3])
 {
   //Z = out[0]
   //almostLog = out[1] & out[2]
-
-	unsigned char invtable0[] = {32,32,31,30,29,28,27,27,26,25,25,24,24,23,23,22,43,42,41,41,40,39,38,38,37,36,36,35,35,34,34,33};
-	unsigned long int logtable0_1[] = {0,0,266327,541388,825775,1120142,1425216,1425216,1741805,2070812,2070812,2413252,2413252,2770268,2770268,3143156,5910074,6107462,6309607,6309607,6516744,6729125,6947023,6947023,7170733,7400572,7400572,7636886,7636886,7880051,7880051,8130476};
-	unsigned long int logtable1_1[] = {0,16400,49296,82322,115479,148767,182188,215742,232570,266327,300220,334251,368421,402730,437180,471772};
-	unsigned long int logtable2_1[] = {0,2048,6146,10246,14348,18452,22558,26666,30776,34888,39002,43118,47236,51356,55479,59603};
-	unsigned long int logtable3_1[] = {0,256,768,1280,1792,2304,2816,3328,3840,4353,4865,5377,5890,6402,6914,7427};
+  //static unsigned long long int out[3];
+	unsigned char invtable0[] = { 32,32,31,30,29,28,27,27,26,25,25,24,24,23,23,22,43,42,41,41,40,39,38,38,37,36,36,35,35,34,34,33 };
+	unsigned long int logtable0_1[] = { 0,0,266327,541388,825775,1120142,1425216,1425216,1741805,2070812,2070812,2413252,2413252,2770268,2770268,3143156,5910074,6107462,6309607,6309607,6516744,6729125,6947023,6947023,7170733,7400572,7400572,7636886,7636886,7880051,7880051,8130476 };
+	unsigned long int logtable1_1[] = { 0,16400,49296,82322,115479,148767,182188,215742,232570,266327,300220,334251,368421,402730,437180,471772 };
+	unsigned long int logtable2_1[] = { 0,2048,6146,10246,14348,18452,22558,26666,30776,34888,39002,43118,47236,51356,55479,59603 };
+	unsigned long int logtable3_1[] = { 0,256,768,1280,1792,2304,2816,3328,3840,4353,4865,5377,5890,6402,6914,7427 };
 	unsigned long int logtable4_1[] = {0,32,96,160,224,288,352,416,480,544,608,672,736,800,864,928};
-	unsigned long int logtable5_1[] = {0,4,12,20,28,36,44,52,60,68,76,84,92,100,108,116};
+	unsigned long int logtable5_1[] = { 0,4,12,20,28,36,44,52,60,68,76,84,92,100,108,116 };
 
 	unsigned long long int logtable0_0[] = {0, 0, 443495784957107441, 408967542716309484, 210202761557502709, 586169748105148596, 483367143421258030, 483367143421258030, 272658672494231207, 489550094456689689, 489550094456689689, 154982152445328750, 154982152445328750, 65043238028969363, 65043238028969363, 539027481613361078, 622583452284905181, 759572586635749126, 660371207705242522, 660371207705242522, 253985390270980735, 446061511024831737, 645570292658782688, 645570292658782688, 35466593504830009, 328384990975929281, 328384990975929281, 840155138376129331, 840155138376129331, 936672260909567195, 936672260909567195, 712430320143961608};
 	unsigned long long int logtable1_0[] = {0, 24054437449941404, 651381705105933614, 718720183519062944, 253546689982371970, 450311980697035046, 211900181597358425, 761543230352098123, 421132014018166394, 443495784957107441, 918228965153013894, 815353360150571272, 273403381874430950, 599681119542337355, 811752134397187310, 1096475649311993935};
@@ -42,24 +51,30 @@ void range_red(unsigned char A, unsigned long long int Y0, unsigned long long in
 	unsigned long long int logtable8[] = {0,9007199258935296,27021597801971712,45035996378562560,63050394988707840,81064793632407553,99079192309661699,117093591020470277,135107989764833288,153122388542750732,171136787354222609,189151186199248920,207165585077829663,225179983989964840,243194382935654451,261208781914898495};
 
 
-	unsigned char A0,A1,A2,A3,A4,A5,A6,A7,A8;
-	unsigned long long int B0,B1,B2,B3,B4,B5,B6,B7,B8;
-	unsigned long long int P0,P1,P2,P3,P4,P5,P6,P7,P8;
-
-	Z0 : Z9 ; //53-69-54 bits
-	Z0_d : Z9_d ; //53-69-54 bits
-	epsz1 : epsZ8 ; //59-84bits
+	unsigned char A0, A1, A2, A3, A4, A5, A6, A7, A8;
+	unsigned long long int B0, B1, B2, B3_0, B4_0, B5_0, B6, B7, B8;
+	unsigned int B3_1, B4_1, B5_1;
+	unsigned long long int P0, P1, P2_0, P3_0, P4_0, P5, P6, P7, P8;
+	unsigned int P2_1, P3_1, P4_1;
+	unsigned long long int ZM0, ZM1, ZM2, ZM3, ZM4, ZM5, ZM6, ZM7, ZM8;
+	unsigned long long int epsZ1, epsZ2_0, epsZ3_0, epsZ4_0, epsZ5_0, epsZ6_0, epsZ7_0, epsZ8_0;
+	unsigned long int epsZ2_1, epsZ3_1, epsZ4_1, epsZ5_1, epsZ6_1, epsZ7_1, epsZ8_1;
+	unsigned long long int Z, Z1, Z2, Z3_0, Z4_0, Z5_0, Z6_0, Z7_0, Z8_0, Z9;
+	unsigned long int Z3_1, Z4_1, Z5_1, Z6_1, Z7_1, Z8_1;
+	unsigned long long int Z1_d, Z2_d, Z3_d_0, Z4_d_0, Z5_d_0, Z6_d, Z7_d, Z8_d;
+	unsigned long int Z3_d_1, Z4_d_1, Z5_d_1;
 	unsigned char InvA0;
+
 	A0 = A;
 	InvA0 = invtable0[A0];
 	P0 = InvA0 * Y0;
 
-	Z1_d = SELECT_RANGE(P0,53,0);
-	
-	A1 = SELECT_RANGE(Z1_d,53,50);
-	B1 = SELECT_RANGE(Z1_d,49,0);
+	Z1_d = SELECT_RANGE(P0, 53, 0);
+
+	A1 = SELECT_RANGE(Z1_d, 53, 50);
+	B1 = SELECT_RANGE(Z1_d, 49, 0);
 	ZM1 = Z1_d;
-	P1 = A1*ZM1;
+	P1 = A1 * ZM1;
 
 	//epsZ1 = SELECT_BIT(A1,3)==0 ? (SELECT_BIT(A1,2)==0 ? 0 : ((1<<58) | Z1_d)) : ;
 	/*
@@ -67,17 +82,106 @@ void range_red(unsigned char A, unsigned long long int Y0, unsigned long long in
 	  0XXX : 01 0000 Z1_d
 	  else : 1 0000 Z1_d 0
 	*/
-	epsZ1 = SELECT_RANGE(A1,3,2)==0 ? 0 : (SELECT_BIT(A1,2)==0 ? 0 : ((((unsigned long long)1<<58) | Z1_d)));
-	epsZ1 = SELECT_BIT(A1,3)==0 = epsZ1 : epsz1<<1;
+	epsZ1 = SELECT_RANGE(A1, 3, 2) == 0 ? 0 : (SELECT_BIT(A1, 2) == 0 ? 0 : ((((unsigned long long)1 << 58) | Z1_d)));
+	epsZ1 = SELECT_BIT(A1, 3) == 0 ? epsZ1 : epsZ1 << 1;
 
 	Z2 = (B1<<8) - (P1<<1) + epsZ1;
-	
-	A2 = SELECT_RANGE(Z2_d,59,56);
-	B2 = SELECT_RANGE(Z2_d,55,0);
-	ZM2 = Z2_d;  //this variable is removable
-	P2 = A2 * ZM2 ;
-	//68bits
-	epsZ2 = A2==0 ? 0 : 1<< ;
+
+	A2 = SELECT_RANGE(Z2_d, 59, 56);
+	B2 = SELECT_RANGE(Z2_d, 55, 0);
+	ZM2 = Z2_d;  //this variable is removable but let it stay
+	//P2 = A2 * ZM2 ; 68bits
+	//while Amax=1b1111=15, we can split ZM and P as 60bits and rest
+	P2_0=ZM2*A2;
+	P2_1=(ZM2*A2)+SELECT_RANGE(P2_0,63,60);
+	BIT_RESIZE(P2_0,60);
+	epsZ2_0 = A2==0 ? 0 : Z2_d;
+	epsZ2_1 = A2==0 ? 0 : 1<<7;
+	Z3_0 =
+	Z3_1 =
+
+	Z3_d_0 = Z3_0;
+	Z3_d_1 = Z3_1;
+
+	A3 = SELECT_RANGE(Z3_d_1,8,5); // 68:60-59:0
+	B3_1 = SELECT_RANGE(Z3_d_1,4,0);
+	B3_0 = Z3_d_0;
+
+	ZM3 = (SELECT_RANGE(Z3_d_1,8,0)<<54) | SELECT_RANGE(Z3_d_0,59,6);
+	P3_0 = ZM3 * A3;
+	P3_1=(ZM3*A3)+SELECT_RANGE(P3_0,63,60);
+	BIT_RESIZE(P4_0, 60);
+	epsZ3_0 = A3 == 0 ? 0 : Z3_d_0;
+	epsZ3_1 = A3 == 0 ? 0 : (1 << 19) | Z3_d_1;
+	Z4_0 = NULL;
+	Z4_1 = NULL ;
+
+	Z4_d_0 = Z4_0;
+	Z4_d_1 = Z4_1;
+
+	A4 = SELECT_RANGE(Z4_d_1, 9, 6);
+	B4_1 = SELECT_RANGE(Z4_d_1, 5, 0);
+	B4_0 = Z4_d_0;
+
+	ZM4 = (SELECT_RANGE(Z4_d_1, 9, 0) << 47) | SELECT_RANGE(Z4_d_0, 59, 13);
+	P4_0 = ZM4 * A4;
+	P4_1 = (ZM4 * A4) + SELECT_RANGE(P4_0, 63, 60);
+	BIT_RESIZE(P4_0, 60);
+	epsZ4_0 = A4 == 0 ? 0 : Z4_d_0;
+	epsZ4_1 = A4 == 0 ? 0 : (1 << 22) | Z4_d_1;
+	Z5_0 = NULL;
+	Z5_1 = NULL;
+
+	Z5_d_0 = Z5_0;
+	Z5_d_1 = Z5_1;
+
+	A5 = SELECT_RANGE(Z5_d_1, 6, 3);
+	B5_1 = SELECT_RANGE(Z5_d_1, 2, 0);
+	B5_0 = Z5_d_0;
+
+	ZM5 = (SELECT_RANGE(Z5_d_1, 6, 0) << 44) | SELECT_RANGE(Z5_d_1, 59, 16);
+	P5 = ZM5 * A5; //55bits - no need to split
+	epsZ5_0 = A5 == 0 ? 0 : Z5_d_0;
+	epsZ5_1 = A5 == 0 ? 0 : (1 << 25) | Z5_d_1;
+	Z6_0 = NULL;
+	Z6_1 = NULL;
+
+	Z6_d = (SELECT_RANGE(Z6_1, 3, 0) << 60) | Z6_0;
+
+	A6 = SELECT_RANGE(Z6_d, 63, 60);
+	B6 = SELECT_RANGE(Z6_d, 59, 0);//60bits
+
+	ZM6 = SELECT_RANGE(Z6_d, 63, 19);
+	P6 = A6 * ZM6;
+	epsZ6_0 = A6 == 0 ? 0 : SELECT_RANGE(Z6_d, 59, 0);
+	epsZ6_1 = A6 == 0 ? 0 : (1 << 23) | SELECT_RANGE(Z6_d, 63, 60);
+	Z7_0 = NULL;
+	Z7_1 = NULL;
+
+	Z7_d = (SELECT_BIT(Z7_1, 0) << 60) | Z7_0;
+
+	A7 = SELECT_RANGE(Z7_d, 60, 57);
+	B7 = SELECT_RANGE(Z7_d, 56, 0);
+
+	ZM7 = SELECT_RANGE(Z7_d, 60, 2);
+	P7 = A7 * ZM7;
+	epsZ7_0 = A7 == 0 ? 0 : SELECT_RANGE(Z7_d, 59, 0);
+	epsZ7_1 = A7 == 0 ? 0 : (1 << 23) | SELECT_BIT(Z7_d, 60);
+	Z8_0 = NULL;
+	Z8_1 = NULL;
+
+	Z8_d = Z8_0;
+
+	A8 = SELECT_RANGE(Z8_d, 57, 54);
+	B8 = SELECT_RANGE(Z8_d, 53, 0);
+
+	ZM8 = SELECT_RANGE(Z8_d, 57, 25);
+	P8 = A8 * ZM8;
+	epsZ8_0 = A8 == 0 ? 0 : Z8_d;
+	epsZ8_1 = A8 == 0 ? 0 : (1 << 23);
+	Z9 = NULL;
+
+	Z = Z9;
 
 
 	//Sum part is splitted to 2 part , also the tables are splitted to 2 part
@@ -109,8 +213,11 @@ void range_red(unsigned char A, unsigned long long int Y0, unsigned long long in
 	SUM_MS = SELECT_RANGE(SUM_LS,63,60) + SELECT_RANGE(L7,63,60) + SELECT_RANGE(L6,63,60) + L5_1 + L4_1 + L3_1 + L2_1 + L1_1 + L0_1;
 	BIT_RESIZE(SUM_LS, 60); //
 
+	out[0] = Z;
 	out[1] = SUM_MS;
 	out[2] = SUM_LS;
+
+
 }
 
 
@@ -134,13 +241,13 @@ float ADD_BUILTIN_PREFIX(logf)(float x)
     unsigned char lzc_size = 6;
     _Bool FirstBit, sR, small, doRR, ufl, sticky, round;
     unsigned long long Y0, Log_small_normd, Log_g, EFR, Zfinal, Log1p_normal, Z2o2_full, Log_small, Z_small, Z2o2_small ;
-	unsigned short int E, absE, E_small, ER, E_normal, lzo, shiftval;
-	unsigned int absZ0, absZ0s, squarerIn, Z2o2_small_s, Z2o2;
+		unsigned short int E, absE, E_small, ER, E_normal, lzo, shiftval;
+		unsigned int absZ0, absZ0s, squarerIn, Z2o2_small_s, Z2o2;
     unsigned char E0_sub;
 
     signal absELog2  : std_logic_vector(wF+wE+g-1 downto 0);
     signal absELog2_pad, LogF_normal_pad, Log_normal, Log_normal_normd  : std_logic_vector(wE+targetprec-1 downto 0);
-	unsigned long long int almostLog_M, almostLog_L, logF_normal;// : std_logic_vector(targetprec - 1 downto 0);
+		unsigned long long int almostLog_M, almostLog_L, logF_normal;// : std_logic_vector(targetprec - 1 downto 0);
 
     func_in.f = x;
     fpX = func_in.b;
@@ -170,8 +277,8 @@ float ADD_BUILTIN_PREFIX(logf)(float x)
 //	rr: range_red
 //	port map ( A => fpX(wF-1 downto wF-a0), Y0 => Y0,
 //	Z => Zfinal, almostLog => almostLog);*/
-	
-	
+
+
 	unsigned long long int out[3];
 	unsigned char A = SELECT_RANGE(fpX, 51, 47);
 	range_red(A, Y0, out[3]);
@@ -180,7 +287,7 @@ float ADD_BUILTIN_PREFIX(logf)(float x)
 	almostLog_L = out[2];
 
 
-	 
+
     lshiftsmall: lshift
       generic map (w => wF-pfinal+2,  n => log2wF)
       port map (  i => absZ0, s => shiftval(log2wF-1 downto 0), o => absZ0s );
